67499c85d509d2fbd161f682d616be9037b6c829
Mizraim Eliab <mizraimeliab168@gmail.com>
2021-12-07 14:15:48 -0600

    add test and pages profile

---
+++ Pages/Profile/styledProfileComponent.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+
+export const StyledContainer = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    background: #2A8D45;
+`;
+export const StyledView = styled.View`
+    align-items: center;
+    justify-content: center;
+`;
+export const StyledText = styled.Text`
+    color: black;
+    font-weight: 700;
+    font-size: 40;
+    margin-top: 15;
+`;
